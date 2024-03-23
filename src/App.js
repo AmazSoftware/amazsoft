c4ed5b4e676b41f025e5261a094e282d00e738da
Abhishek Sharma <abhishek.sharma.devind@gmail.com>
2024-03-23 17:16:40 +0530

    fresh commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from "./header";
+import Services from "./services";
+import Tech from "./tech";
+import Features from "./features";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Services/>
+    <Tech/>
+    <Features/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/tech.js
@@ -0,0 +1,142 @@
+import go from "./images/go.png"
+import react from "./images/react.png"
+export default function Tech(){
+    return (
+        
+<div class="relative p-4 bg-white dark:bg-gray-800">
+<br></br><br></br>
+    <h4 class="text-center mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+                Technologies
+    </h4>
+    <br></br>
+    <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-12 lg:items-center">
+        <div class="lg:col-start-2 md:pl-20">
+            <h4 class="text-2xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:leading-9">
+                ReactJS
+            </h4>
+            <ul class="mt-10">
+                <li>
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
+                                <svg width="20" height="20" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                                    </path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <h5 class="text-lg font-bold leading-6 text-gray-900 dark:text-white">
+                            It is a Latest Technology is used for build Web Application.
+                            </h5>
+                        </div>
+                    </div>
+                </li>
+                <li class="mt-10">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
+                                <svg width="20" height="20" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                                    </path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <h5 class="text-lg font-bold leading-6 text-gray-900 dark:text-white">
+                                Able to Make Single Page Application.
+                            </h5>
+                        </div>
+                    </div>
+                </li>
+                <li class="mt-10">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
+                                <svg width="20" height="20" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                                    </path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <h5 class="text-lg font-bold leading-6 text-gray-900 dark:text-white">
+                                Implement TailwindCSS.
+                            </h5>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="relative mt-10 -mx-4 md:-mx-12 lg:mt-0 lg:col-start-1">
+            <img src={react} alt="reactjs" class="relative w-auto mx-auto rounded shadow-lg"/>
+        </div>
+    </div>
+    <br></br>
+    <br></br>
+    <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-12 lg:items-center">
+        <div class="lg:col-start-2 md:pl-20">
+            <h4 class="text-2xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:leading-9">
+                Golang
+            </h4>
+            <ul class="mt-10">
+                <li>
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
+                                <svg width="20" height="20" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                                    </path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <h5 class="text-lg font-bold leading-6 text-gray-900 dark:text-white">
+                                Latest Techonology used for Backend Development.
+                            </h5>
+                        </div>
+                    </div>
+                </li>
+                <li class="mt-10">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
+                                <svg width="20" height="20" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                                    </path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <h5 class="text-lg font-bold leading-6 text-gray-900 dark:text-white">
+                               Able to make Light-weighted Application.
+                            </h5>
+                        </div>
+                    </div>
+                </li>
+                <li class="mt-10">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
+                                <svg width="20" height="20" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                                    </path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <h5 class="text-lg font-bold leading-6 text-gray-900 dark:text-white">
+                                Easy to Manage Multi-processing.
+                            </h5>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="relative mt-10 -mx-4 md:-mx-12 lg:mt-0 lg:col-start-1">
+            <img src={go} alt="golang" class="relative w-auto mx-auto rounded shadow-lg"/>
+        </div>
+    </div>
+</div>
+    );
+}
